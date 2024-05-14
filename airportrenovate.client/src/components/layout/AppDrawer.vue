a79e9836e5495ee6117f28eb2cddb2054acce662
brian <brian0889999@gmail.com>
2024-05-14 18:35:56 +0800

    fear:appbar,drawer...practice

--- airportrenovate.client/src/components/layout/AppDrawer.vue
+++ airportrenovate.client/src/components/layout/AppDrawer.vue
@@ -1,74 +1,73 @@
 <template>
-    <v-card>
-        <v-layout>
-            <!--<v-app-bar color="primary"
-                       prominent>
-                <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
-                <v-toolbar-title>My files</v-toolbar-title>
+    <!--<v-app-bar color="primary"
+               prominent>
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
-                <v-spacer></v-spacer>
+        <v-toolbar-title>My files</v-toolbar-title>
 
-                <template v-if="$vuetify.display.mdAndUp">
-                    <v-btn icon="mdi-magnify" variant="text"></v-btn>
+        <v-spacer></v-spacer>
 
-                    <v-btn icon="mdi-filter" variant="text"></v-btn>
-                </template>
+        <template v-if="$vuetify.display.mdAndUp">
+            <v-btn icon="mdi-magnify" variant="text"></v-btn>
 
-                <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
-            </v-app-bar>-->
+            <v-btn icon="mdi-filter" variant="text"></v-btn>
+        </template>
 
-            <v-navigation-drawer v-model="drawer"
-                                 :location="$vuetify.display.mobile ? 'bottom' : undefined"
-                                 temporary>
-                <v-list :items="items"></v-list>
-            </v-navigation-drawer>
+        <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
+    </v-app-bar>-->
+    <!--temporary-->
+    <v-navigation-drawer v-model="drawer"
+                         :location="$vuetify.display.mobile ? 'bottom' : undefined"
+                         :rail="rail">
+        <v-list :items="items"></v-list>
+    </v-navigation-drawer>
 
-            <v-main style="height: 500px;">
-                <v-card-text>
-                    The navigation drawer will appear from the bottom on smaller size screens.
-                </v-card-text>
-            </v-main>
-        </v-layout>
-    </v-card>
+    <!--<v-main style="height: 500px;">
+        <v-card-text>
+            The navigation drawer will appear from the bottom on smaller size screens.
+        </v-card-text>
+    </v-main>-->
+
 </template>
+<script setup lang="ts">
+    import { ref, computed, watch } from 'vue';
+    import { useStore } from '@/store/index';
 
-<script>
-    export default {
-        data: () => ({
-            drawer: false,
-            group: null,
-            items: [
-                {
-                    title: 'Foo',
-                    value: 'foo',
-                },
-                {
-                    title: 'Bar',
-                    value: 'bar',
-                },
-                {
-                    title: 'Fizz',
-                    value: 'fizz',
-                },
-                {
-                    title: 'Buzz',
-                    value: 'buzz',
-                },
-            ],
-        }),
+/*const drawer = ref(false);*/
 
-        watch: {
-            group() {
-                this.drawer = false
-            },
-        },
-    }
+    const store = useStore();
+const drawer = computed({
+    get: () => store.drawer,
+    set: (val: boolean) => store.setDrawer(val)
+});
+/*const group = ref<string | null>(null);*/
+const items = [
+  {
+    title: 'Foo',
+    value: 'foo',
+  },
+  {
+    title: 'Bar',
+    value: 'bar',
+  },
+  {
+    title: 'Fizz',
+    value: 'fizz',
+  },
+  {
+    title: 'Buzz',
+    value: 'buzz',
+  },
+];
+
+//watch(group, () => {
+//  drawer.value = false;
+//});
 </script>
 
-<!--<script setup lang="ts">
-
-</script>-->
-
 <style scoped>
+    .v-navigation-drawer__scrim {
+        display: none;
+    }
 </style>
--- airportrenovate.client/src/main.ts
+++ airportrenovate.client/src/main.ts
@@ -1,13 +1,18 @@
 import './assets/main.css';
 
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 /*import axios from 'axios';*/
 import vuetify from './plugins/vuetify';
 
+const app = createApp(App)
+const pinia = createPinia();
 
 
-
-createApp(App).use(router).use(vuetify).mount('#app');
+app.use(pinia)
+    .use(router)
+    .use(vuetify)
+    .mount('#app');
 /*createApp(App).mount('#app')*/
